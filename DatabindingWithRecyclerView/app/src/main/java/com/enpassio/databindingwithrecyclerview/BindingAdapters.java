0b94cbaec7cad71ad578fdb3751125f5b444e4df
Oya Canli <oyacan@gmail.com>
2018-11-23 10:11:20 +0100

    add glide and corresponding binding adapter

--- DatabindingWithRecyclerView/app/src/main/java/com/enpassio/databindingwithrecyclerview/BindingAdapters.java
+++ DatabindingWithRecyclerView/app/src/main/java/com/enpassio/databindingwithrecyclerview/BindingAdapters.java
@@ -3,11 +3,29 @@ package com.enpassio.databindingwithrecyclerview;
 import androidx.databinding.BindingAdapter;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
 public class BindingAdapters {
 
     @BindingAdapter("android:src")
     public static void setImageUrl(ImageView view, int resourceId) {
+        //This is for turning off default transformations
+        RequestOptions options = new RequestOptions();
+        options.dontTransform();
+
+        Glide.with(view.getContext())
+                .load(resourceId)
+                .apply(options)
+                .into(view);
+    }
+
+    /////////////// WITHOUT GLIDE: ////////////////////////
+
+    /*@BindingAdapter("android:src")
+    public static void setImageUrl(ImageView view, int resourceId) {
         //Get the drawable from the resource id and set it to the image view.
         view.setImageDrawable(view.getContext().getResources().getDrawable(resourceId));
-    }
+    }*/
+
 }
