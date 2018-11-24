3ba5dc3d9312a852ee163389b7c8da200a84cd82
Oya Canli <oyacan@gmail.com>
2018-11-24 18:59:05 +0100

    remove glide

--- DatabindingWithRecyclerView/app/src/main/java/com/enpassio/databindingwithrecyclerview/utils/BindingAdapters.java
+++ DatabindingWithRecyclerView/app/src/main/java/com/enpassio/databindingwithrecyclerview/utils/BindingAdapters.java
@@ -1,31 +1,14 @@
 package com.enpassio.databindingwithrecyclerview.utils;
 
-import androidx.databinding.BindingAdapter;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
+import androidx.databinding.BindingAdapter;
 
 public class BindingAdapters {
 
     @BindingAdapter("android:src")
     public static void setImageUrl(ImageView view, int resourceId) {
-        //This is for turning off default transformations
-        RequestOptions options = new RequestOptions();
-        options.dontTransform();
-
-        Glide.with(view.getContext())
-                .load(resourceId)
-                .apply(options)
-                .into(view);
-    }
-
-    /////////////// WITHOUT GLIDE: ////////////////////////
-
-    /*@BindingAdapter("android:src")
-    public static void setImageUrl(ImageView view, int resourceId) {
         //Get the drawable from the resource id and set it to the image view.
         view.setImageDrawable(view.getContext().getResources().getDrawable(resourceId));
-    }*/
-
+    }
 }
