6f7592ed5ac4342fc083b554a963daa059d7ab58
Oya Canli <oyacan@gmail.com>
2018-12-25 23:13:02 +0100

    add glide module and placeholder image

--- DatabindingWithNewsApi/app/src/main/java/com/enpassio/databindingwithnewsapi/BindingUtils.java
+++ DatabindingWithNewsApi/app/src/main/java/com/enpassio/databindingwithnewsapi/BindingUtils.java
@@ -4,14 +4,13 @@ import android.databinding.BindingAdapter;
 import android.text.TextUtils;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
-
 public final class BindingUtils {
 
     @BindingAdapter("android:src")
     public static void setImageUrl(ImageView view, String url) {
-        Glide.with(view.getContext())
+        GlideApp.with(view.getContext())
                 .load(url)
+                .error(R.drawable.image_not_found)
                 .into(view);
     }
 
---
+++ DatabindingWithNewsApi/app/src/main/java/com/enpassio/databindingwithnewsapi/NewsAppModule.java
@@ -0,0 +1,8 @@
+package com.enpassio.databindingwithnewsapi;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class NewsAppModule extends AppGlideModule {
+}
