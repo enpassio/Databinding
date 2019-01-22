82f8de11f74c9480bac48c413e137b7f62439ed6
Oya Canli <oyacan@gmail.com>
2019-01-22 19:38:43 +0100

    update gradle, add credit to readme, erase binding adapter, rename utility file

---
+++ DatabindingWithRecyclerView/README.md
@@ -0,0 +1,5 @@
+# Databinding
+Various examples on databinding which can help not only understand how to get started with it but it also shows how this can be used in complex and real use cases
+
+#### Credits:
+Icons in the DatabindingWithRecyclerview sample are made by Freepik from www.flaticon.com
--- DatabindingWithRecyclerView/app/src/main/java/com/enpassio/databindingwithrecyclerview/utils/BindingUtils.java
+++ DatabindingWithRecyclerView/app/src/main/java/com/enpassio/databindingwithrecyclerview/utils/BindingUtils.java
@@ -2,7 +2,7 @@ package com.enpassio.databindingwithrecyclerview.utils;
 
 import java.text.NumberFormat;
 
-public final class DatabindingUtils {
+public final class BindingUtils {
 
     //This method gets the price as an integer and adds a currency sign before it
     public static String addCurrencySign(int price){
