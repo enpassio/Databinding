e9306153a4a4c09e46daad7bc13e660d113003d2
Oya Canli <oyacan@gmail.com>
2019-01-26 20:04:51 +0100

    change binding implementation of details fragment : pass viewModel to the binding implementation instead of the chosen article and use a method reference for read more button.

--- DatabindingWithNewsApi/app/src/main/java/com/enpassio/databindingwithnewsapi/viewmodel/MainViewModel.java
+++ DatabindingWithNewsApi/app/src/main/java/com/enpassio/databindingwithnewsapi/viewmodel/MainViewModel.java
@@ -4,29 +4,33 @@ import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
+import android.content.Intent;
 import android.databinding.ObservableBoolean;
+import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 
 import com.enpassio.databindingwithnewsapi.model.Article;
 import com.enpassio.databindingwithnewsapi.repository.NewsRepository;
 
 import java.util.List;
 
+import static android.support.constraint.Constraints.TAG;
+
 public class MainViewModel extends AndroidViewModel {
 
     private LiveData<List<Article>> mArticleList;
     private final MutableLiveData<Article> mChosenArticle = new MutableLiveData<>();
     private final NewsRepository mRepo;
-    public final ObservableBoolean isLoading = new ObservableBoolean();
-    public final ObservableBoolean networkConnected = new ObservableBoolean();
+    public final ObservableBoolean isLoading = new ObservableBoolean(true);
+    public final ObservableBoolean networkConnected = new ObservableBoolean(true);
 
     public MainViewModel(@NonNull Application application, NewsRepository.NetworkStateListener listener) {
         super(application);
         //Passing the application context to the repository (not activity context!)
         mRepo = NewsRepository.getInstance(this.getApplication(), listener);
-        //Set default values
-        isLoading.set(true);
-        networkConnected.set(true);
     }
 
     public LiveData<List<Article>> getArticleList() {
@@ -46,6 +50,30 @@ public class MainViewModel extends AndroidViewModel {
 
     public void checkConnectionAndStartLoading() {
         mRepo.checkConnectionAndStartFetching();
+    }
+
+    public void openWebSite(View view) {
+        Article chosenArticle = mChosenArticle.getValue();
+        if (chosenArticle == null) {
+            return;
+        }
+        String articleUrl = chosenArticle.getArticleUrl();
+        Uri webUri = null;
+        if (!TextUtils.isEmpty(articleUrl)) {
+            //Parse string to uri
+            try {
+                webUri = Uri.parse(articleUrl);
+            } catch (Exception e) {
+                Log.e(TAG, e.toString());
+            }
+            //Send an implicit intent to open the article in the browser
+            Intent webIntent = new Intent(Intent.ACTION_VIEW);
+            webIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            webIntent.setData(webUri);
+            if (webIntent.resolveActivity(view.getContext().getPackageManager()) != null) {
+                view.getContext().startActivity(webIntent);
+            }
+        }
     }
 
 }
