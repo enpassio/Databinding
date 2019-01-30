97ef12eac24877b9b60a6fc385a13640aa6e6e16
Oya Canli <oyacan@gmail.com>
2019-01-30 14:34:23 +0100

    initial commit for two-way databinding

---
+++ TwoWayDatabinding/app/src/main/java/com/enpassio/twowaydatabinding/MainActivity.java
@@ -0,0 +1,19 @@
+package com.enpassio.twowaydatabinding;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.main_container, new ToyListFragment())
+                    .commit();
+        }
+    }
+}
