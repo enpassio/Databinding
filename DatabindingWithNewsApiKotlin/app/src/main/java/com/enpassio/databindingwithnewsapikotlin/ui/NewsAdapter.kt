e112b9d9f72bd2576241b91d8042c080e2d335f2
Oya Canli <oyacan@gmail.com>
2019-03-11 11:49:44 +0100

    newsapi kotlin: add kotlin version of the newsapi java sample

---
+++ DatabindingWithNewsApiKotlin/app/src/main/java/com/enpassio/databindingwithnewsapikotlin/ui/NewsAdapter.kt
@@ -0,0 +1,47 @@
+package com.enpassio.databindingwithnewsapikotlin.ui
+
+import android.databinding.DataBindingUtil
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.enpassio.databindingwithnewsapikotlin.data.Article
+import com.enpassio.databindingwithnewsapikotlin.R
+import com.enpassio.databindingwithnewsapikotlin.databinding.ItemBinding
+
+
+class NewsAdapter (private val mListener: ArticleClickListener) :
+    RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
+
+    var articleList: List<Article>? = null
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        val binding = DataBindingUtil
+            .inflate<ItemBinding>(LayoutInflater.from(parent.context), R.layout.item_news_article,
+                parent, false)
+        //Pass an item click listener to each item layout.
+        binding.articleItemClick = mListener
+        return NewsViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        //For each item, corresponding product object is passed to the binding
+        holder.binding.article = articleList?.get(position)
+        //This is to force bindings to execute right away
+        holder.binding.executePendingBindings()
+    }
+
+    override fun getItemCount(): Int {
+        //If list is null, return 0, otherwise return the size of the list
+        return articleList?.size ?: 0
+    }
+
+    inner class NewsViewHolder(val binding: ItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    interface ArticleClickListener {
+        fun onArticleClicked(chosenArticle: Article)
+    }
+}
---
+++ DatabindingWithNewsApiKotlin/app/src/main/java/com/enpassio/databindingwithnewsapikotlin/utils/Constants.kt
@@ -0,0 +1,29 @@
+package com.enpassio.databindingwithnewsapikotlin.utils
+
+import com.enpassio.databindingwithnewsapikotlin.BuildConfig
+import com.enpassio.databindingwithnewsapikotlin.BuildConfig.NEWS_API_DOT_ORG_KEY
+
+
+// Constants and keys related to News Api
+const val ARTICLES_ARRAY = "articles"
+const val NEWS_API_KEY = "apiKey"
+const val NEWS_API_VALUE = BuildConfig.NEWS_API_DOT_ORG_KEY
+const val ENDPOINT = "top-headlines"
+const val PAGE_SIZE_PARAM = "pageSize"
+const val COUNTRY = "country"
+const val CATEGORY = "category"
+const val SOURCE = "source"
+const val SOURCE_NAME = "name"
+const val AUTHOR = "author"
+const val TITLE = "title"
+const val DESCRIPTION = "description"
+const val IMAGE_URL = "urlToImage"
+const val ARTICLE_URL = "url"
+const val PUBLISHING_TIME = "publishedAt"
+const val ARTICLE_BODY = "content"
+
+//Sample values used for building a url, which can be changed
+const val BASE_URL = "https://newsapi.org/v2/"
+const val SAMPLE_PAGE_SIZE = "10"
+const val SAMPLE_CATEGORY = "general"
+const val SAMPLE_COUNTRY = "gb"
