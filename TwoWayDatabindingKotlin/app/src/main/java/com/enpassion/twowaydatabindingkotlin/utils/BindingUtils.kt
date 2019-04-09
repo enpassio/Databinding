4e3ac16dd12b095a2b4023e4b8174362a00dcf57
Oya Canli <oyacan@gmail.com>
2019-04-09 17:22:34 +0200

    two-way kotlin : add new icons

--- TwoWayDatabindingKotlin/app/src/main/java/com/enpassion/twowaydatabindingkotlin/utils/BindingUtils.kt
+++ TwoWayDatabindingKotlin/app/src/main/java/com/enpassion/twowaydatabindingkotlin/utils/BindingUtils.kt
@@ -2,29 +2,31 @@
 package com.enpassion.twowaydatabindingkotlin.utils
 
 import android.databinding.BindingAdapter
-import android.graphics.drawable.ColorDrawable
-import android.support.v4.content.ContextCompat
 import android.view.View
 import android.widget.ImageView
 import com.enpassion.twowaydatabindingkotlin.R
 
 
+private const val UNISEX = 0
+private const val GIRL = 1
+private const val BOY = 2
+
 @BindingAdapter("genderDrawable")
 fun getGenderDrawable(imageView: ImageView, position: Int) {
-    when (position) {
-        0 -> imageView.setImageResource(R.drawable.ic_rainbow)
-        1 -> imageView.setImageDrawable(
-                ColorDrawable(ContextCompat.getColor(imageView.context, R.color.color_girls)))
-        2 -> imageView.setImageDrawable(
-                ColorDrawable(ContextCompat.getColor(imageView.context, R.color.color_boys)))
+    val resourceId = when (position) {
+        UNISEX -> R.drawable.ic_rainbow
+        GIRL -> R.drawable.ic_girl
+        BOY -> R.drawable.ic_boy
+        else -> R.drawable.ic_rainbow
     }
+    imageView.setImageResource(resourceId)
 }
 
 @BindingAdapter("stateDrawable")
 fun getStateDrawable(imageView: ImageView, buttonId: Int) {
     when (buttonId) {
-        R.id.radioBtn_new -> imageView.setImageResource(R.drawable.ic_gift)
-        R.id.radioBtn_used -> imageView.setImageResource(R.drawable.ic_recycle)
+        R.id.radioBtn_bought -> imageView.setImageResource(R.drawable.ic_money)
+        R.id.radioBtn_received -> imageView.setImageResource(R.drawable.ic_gift)
     }
 }
 
